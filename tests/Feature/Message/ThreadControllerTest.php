a40057a36030411c56736320626ec84534832fdc
Rex Tangle <svarndell@gmail.com>
2022-11-08 21:00:11 +0000

    add thread tests

--- tests/Feature/Message/ThreadControllerTest.php
+++ tests/Feature/Message/ThreadControllerTest.php
@@ -50,4 +50,45 @@ class ThreadControllerTest extends TestCase
         $response->assertStatus(Response::HTTP_OK);
         $response->assertJsonFragment(['title' => 'Testing Thread Updated']);
     }
+
+    public function test_a_user_cannot_update_someone_elses_threads()
+    {
+        User::factory()->hasThreads(1)->create();
+        User::factory()->create();
+
+        $this->apiLogin(User::find(2));
+
+        $response = $this->patchJson(route('thread.update', 1), [
+            'title' => 'Testing Thread Updated'
+        ]);
+
+        $response->assertStatus(Response::HTTP_FORBIDDEN);
+        $this->assertDatabaseMissing('threads', ['title' => 'Testing Thread Updated']);
+    }
+
+    public function test_a_user_can_delete_threads()
+    {
+        User::factory()->hasThreads(1, ['title' => 'Test Delete Thread'])->create();
+
+        $this->apiLogin();
+
+        $response = $this->deleteJson(route('thread.delete', 1));
+
+        $response->assertStatus(Response::HTTP_OK);
+        $response->assertJsonFragment(['message' => 'Message Deleted']);
+        $this->assertDatabaseMissing('threads', ['title' => 'Test Delete Thread']);
+    }
+
+    public function test_a_user_cannot_delete_someone_elses_threads()
+    {
+        User::factory()->hasThreads(1, ['title' => 'Test Thread'])->create();
+        User::factory()->create();
+
+        $this->apiLogin(User::find(2));
+
+        $response = $this->deleteJson(route('thread.delete', 1));
+
+        $response->assertStatus(Response::HTTP_FORBIDDEN);
+        $this->assertDatabaseHas('threads', ['title' => 'Test Thread']);
+    }
 }
